8146f220d097521ea8b1c5cb2e31c36f57a30e19
Sachin <Sachin@TYSS-Sachin>
2023-03-30 00:26:33 +0530

    1st commit

---
+++ src/gitPractiseProj/test1.java
@@ -0,0 +1,13 @@
+package gitPractiseProj;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=1;
+     int b=2;
+     int c=a+b;
+     System.out.println(c);
+	}
+
+}
